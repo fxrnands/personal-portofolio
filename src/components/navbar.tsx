dc3602362ea2e854119ebd9c071a83ade2da768f
M. Fernanda Kholil Jibron <110803660+fxrnands@users.noreply.github.com>
2023-03-07 01:53:05 +0700

    fix update 1.2

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,19 +1,9 @@
-import { useState } from "react";
-import { Dialog } from "@headlessui/react";
 import Image from "next/image";
 import { FiDownload } from "react-icons/fi";
 
-const navigation = [
-  { name: "Experience", href: "#" },
-  { name: "Projects", href: "#" },
-  { name: "Contact Me", href: "#" },
-];
-
 export default function NavigationBar() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
   return (
-    <header className="bg-gray-900">
+    <header className="bg-gray-900 fixed top-0 w-full z-50">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
@@ -29,73 +19,18 @@ export default function NavigationBar() {
             />
           </a>
         </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-          </button>
-        </div>
-        {/* <div className="hidden ml-10 flex-1 lg:flex lg:gap-x-12">
-          {navigation.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className="text-sm font-semibold leading-6 text-white"
-            >
-              {item.name}
-            </a>
-          ))}
-        </div> */}
         <div className="flex-1 flex justify-end lg:flex-1 lg:gap-x-12">
           <button
             type="button"
             className="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-800 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             <FiDownload className="-ml-0.5 h-5 w-5" aria-hidden="true" />
-            <a href="https://drive.google.com/file/d/1V3LboTXEU1Y1LqHH8xItUPfVR6KlyOsA/view?usp=sharing"> Download CV</a>
+            <a href="https://drive.google.com/file/d/1V3LboTXEU1Y1LqHH8xItUPfVR6KlyOsA/view?usp=sharing">
+              Download CV
+            </a>
           </button>
         </div>
       </nav>
-      <Dialog
-        as="div"
-        className="lg:hidden"
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-      >
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-            </a>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-400"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/25">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-white hover:bg-gray-800"
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
     </header>
   );
 }
